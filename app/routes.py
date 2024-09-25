9a97dde39e15ac880618752af2b5802f5a935f6a
lmw2109 <lucas_wagner@me.com>
2024-09-25 15:50:08 +0200

    Added user area

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, flash
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 
 import sqlalchemy as sa
 
@@ -10,6 +10,9 @@ from app.models import User
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/index", methods=['GET', 'POST'])
 def index():
+    # if already signed in redirect
+    if current_user.is_authenticated:
+        return redirect(url_for("projects", username=current_user.username))
     title = "Hello, Visitor!"
     message = "This will be an innovative ToDo WebApp using Python Flask!"
     return render_template("index.html", title = title, message = message)
@@ -18,7 +21,7 @@ def index():
 def signin():
     # if already signed in
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for("projects", username=current_user.username))
     # create form
     form = SigninForm()
     title = "Sign In"
@@ -32,7 +35,7 @@ def signin():
             flash("Invalid password")
         else: # sign in
             login_user(user, remember=form.remember.data)
-            return redirect(url_for("index"))
+            return redirect(url_for("projects", username=current_user.username))
         return redirect(url_for("signin"))
     return render_template("signin.html", title=title, form=form)
 
@@ -45,7 +48,7 @@ def signout():
 def signup():
     # if already signed in redirect
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for("projects", username=current_user.username))
     # create form
     form = SignupForm()
     title = "Sign Up"
@@ -55,5 +58,17 @@ def signup():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('signin'))
-    return render_template('signup.html', title=title, form=form)
+        return redirect(url_for("signin"))
+    return render_template("signup.html", title=title, form=form)
+
+@app.route("/profile/<username>")
+@login_required
+def profile(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    return render_template("profile.html", user=user)
+
+@app.route("/projects/<username>")
+@login_required
+def projects(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    return render_template("projects.html", user=user)
