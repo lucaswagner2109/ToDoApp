1db506e4b46f1feef76147950cf418e0789eb2c7
lmw2109 <lucas_wagner@me.com>
2024-09-25 12:15:15 +0200

    User sign up and sign in

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from app.config import Config
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+signin = LoginManager(app)
+signin.signin_view = "signin"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
+from flask_login import current_user, login_user, logout_user
 
-from app import app
-from app.forms import SigninForm
+import sqlalchemy as sa
+
+from app import app, db
+from app.forms import SigninForm, SignupForm
+from app.models import User
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/index", methods=['GET', 'POST'])
@@ -12,10 +16,44 @@ def index():
 
 @app.route("/signin", methods=['GET', 'POST'])
 def signin():
+    # if already signed in
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     # create form
     form = SigninForm()
     title = "Sign In"
     # validate input
     if form.validate_on_submit():
-        return redirect("/index")
-    return render_template("signin.html", title = title, form=form)
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None:
+            flash("Invalid username")
+        elif not user.check_password(form.password.data):
+            flash("Invalid password")
+        else: # sign in
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for("index"))
+        return redirect(url_for("signin"))
+    return render_template("signin.html", title=title, form=form)
+
+@app.route("/signout")
+def signout():
+    logout_user()
+    return redirect(url_for("index"))
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    # if already signed in redirect
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    # create form
+    form = SignupForm()
+    title = "Sign Up"
+    # validate
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('signin'))
+    return render_template('signup.html', title=title, form=form)
